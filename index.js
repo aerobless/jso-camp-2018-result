0085b39f6469aebd21c2b3d1a639c806246bec43
Theo Winter <theo.winter@gmail.com>
2018-06-28 08:52:40 +0200

    Added better routing & test for mysql access

--- index.js
+++ index.js
@@ -1,19 +1,41 @@
 const router = require('gcf-api-router')();
+const mysql = require('mysql');
+const request = require('request');
 
-router.route('/kickerbox')
-    .get(getKickerboxes);
+const connectionName = 'jso-camp:europe-west1:kickerbox-cf';
+const dbUser = 'kickboxer';
+const dbPass = ''; //replace me
+const dbName = 'kickerbox';
 
-router.route('/kickerbox/:id')
-    .get(getKickerbox);
+const test = getTest('test');
 
-router.route('/result')
-    .get(getResults)
-    .post(postResult);
 
-router.route('/result/:id')
-    .get(getResult);
+function getTest(name) {
+    request('http://www.google.com', function (error, response, body) {
+        console.log('error:', error); // Print the error if one occurred
+        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        console.log('body:', body); // Print the HTML for the Google homepage.
+        return body;
+    });
+}
+
+router.route('/kickerbox').get(getKickerboxes);
+router.route('/kickerbox/:id').get(getKickerbox);
+
+router.route('/result').get(getResults).post(postResult);
+router.route('/result/:id').get(getResult);
+
+router.route('/test').get(testDb);
 
 router.notFound(handleError);
+
+const pool = mysql.createPool({
+    connectionLimit: 1,
+    socketPath: '/cloudsql/' + connectionName,
+    user: dbUser,
+    password: dbPass,
+    database: dbName
+});
 
 let kickerboxes = [
     {id: 0, name: 'PrettyKickerbox', location: 'Zühlke Tower', model: '1337-ABC'},
@@ -34,7 +56,13 @@ let notFoundMsg = {
 
 function handleError(req, res) {
     res.status(404);
-    res.send('Endpoint not found');
+    res.setHeader('content-type', 'text/html');
+    res.send('<b>Usage:</b></br>\n' +
+        '   GET  /api/kickerbox       returns all kickerboxes</br> \n' +
+        '   GET  /api/kickerbox/:id   returns a specific kickerbox</br></br> \n' +
+        '   GET  /api/result          returns all results</br> \n' +
+        '   GET  /api/result/:id      returns a specific result</br> \n' +
+        '   POST /api/result/         post a new result</br>');
 }
 
 function getKickerboxes(req, res) {
@@ -54,6 +82,27 @@ function getKickerbox(req, res) {
 
 function getResults(req, res) {
     res.send(mockedResults);
+}
+
+function testDb(req, res) {
+
+    res.send(test);
+
+    /*request('http://metadata.google.internal/computeMetadata/v1/project/attributes/db-name" -H "Metadata-Flavor: Google', {json: true}, (err, res, body) => {
+        if (err) {
+            console.log(err);
+            response = err;
+        } else {
+            console.log(body);
+            response = body;
+        }
+    });*/
+
+    pool.query('SELECT NOW() AS now', (error, results, fields) => {
+        //callback(error, results);
+        res.send(results);
+        //res.send(results);
+    });
 }
 
 function getResult(req, res) {
